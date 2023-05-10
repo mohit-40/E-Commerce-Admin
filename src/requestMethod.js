61700c8726610be1328c9c353b6d2aa2bebcd971
Mohit agarawal <2019285@iiitdmj.ac.in>
2023-05-10 23:15:43 +0530

    made the build folder

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 let BASE_URL = "http://localhost:8800/api/"
 if (process.env.NODE_ENV === "production") {
-	BASE_URL = "https://shophify.herokuapp.com/api/";
+	BASE_URL = "https://fashionstore.cyclic.app/api/";
 }
 
 export const publicRequest = axios.create({ baseURL: BASE_URL })
